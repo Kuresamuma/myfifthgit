12943ee5dad4fe1c46a707bf3c15f8a62fc924df
kuresamuma <umakuresam@gmail.com>
2022-12-27 00:05:33 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def uma(request):
+    return HttpResponse("<i>i love myself</i>")
+def uma(request):
+    return HttpResponse("<marquee>i love my parents</marquee>")
---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def uma(request):
+    return HttpResponse("I LOVE MY MOM")
+def uma(request):
+    return HttpResponse("BE HAPPY AND MAKE OTHERS ALSO HAPPY")
